0f5a29c3aea12f74c2bdd4ab17566d0818541124
Deekshith <deekshith@prakash-ka-MacBook-Pro.local>
2026-02-25 16:05:59 +0530

    bugs fixed for layout

--- template2/src/components/Layout.tsx
+++ template2/src/components/Layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col bg-offwhite">
       <Navbar />
-      <main className="flex-grow overflow-x-hidden">{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
       <EnquireNowFormModal />
       <ScrollToTopButton />
